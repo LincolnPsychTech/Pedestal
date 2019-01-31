0f49f3b030d3d08778724325492cbae337e622f9
Todd Parsons <todd.e.parsons@googlemail.com>
2019-01-31 16:44:11 +0000

    loadobj function on Procedure, minor changes, major structural changes to central interface

--- Placeholder.m
+++ Placeholder.m
@@ -1,6 +1,12 @@
 Pedestal
-% ex = Experiment;
+% ex = Procedure;
 % de = Design;
 % rt = Routine;
 % em = Element;
 % f = Factors;
+
+i = strcmp({pr.Tag}, 'For');
+parent = [pr(i).Children];
+
+i = strcmp({nodes.Tag}, 'For');
+parent = [nodes(i).Children];
