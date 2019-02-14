83a17d15e1663397ee7e6f2a735aa0c2b1ee3d71
Todd Parsons <todd.e.parsons@googlemail.com>
2019-02-14 17:06:53 +0000

    Finalising save/load functions (unfinished)

--- System/Placeholder.m
+++ System/Placeholder.m
@@ -4,9 +4,3 @@ Pedestal
 % rt = Routine;
 % em = Element;
 % f = Factors;
-
-i = strcmp({pr.Tag}, 'For');
-parent = [pr(i).Children];
-
-i = strcmp({nodes.Tag}, 'For');
-parent = [nodes(i).Children];
