e9732af5b3a7176b3897fda6dc1bb443d1ce912c
Todd Parsons <todd.e.parsons@googlemail.com>
2020-04-28 14:54:44 +0100

    Changes to window drawing

--- System/Compile/pedwindow.m
+++ System/Compile/pedwindow.m
@@ -24,7 +24,7 @@ fig = figure(... % Create full screen figure
 ax = axes(fig, ... % Create axis within figure
     'Position', [0 0 1 1], ... % Occupy entire figure
     'XLim', sDim([1,3]), ... % X limits are width of screen in pixels
-    'YLim', sDim([1,3]), ... % Y limits are height of screen in pixels
+    'YLim', sDim([2,4]), ... % Y limits are height of screen in pixels
     'Color', col, ... % Colour defined by user
     'TickLength', [0 0], ... % Remove ticks
     'Box', 'off' ... % Remove box
@@ -55,7 +55,7 @@ ax = axes(fig, ... % Create axis within figure
     end
 
     function closeFcn(app, ~)
-        assignin('base', 'data', app.UserData.Store);
+        assignin('base', 'data_backup', app.UserData.Store);
         disp('Experiment terminated by user.');
         delete(app)
     end
